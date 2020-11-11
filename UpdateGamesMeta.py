68dc6d94cbbe682fa8a366efacbacde0511d8f02
Marco <>
2020-11-11 10:41:36 +0000

    UpdateGamesMeta now stores the popularity filter array

--- UpdateGamesMeta.py
+++ UpdateGamesMeta.py
@@ -7,6 +7,7 @@ import sys
 import functools
 from Mongo import Mongo
 from Shared import settings, hasTestArgs
+import math
 import Helpers
 
 class UpdateGamesMeta:
@@ -16,19 +17,20 @@ class UpdateGamesMeta:
 
     def start(self):
         self.updateTagsRecord()
+        self.updatePopularityIntervals()
 
     def sortFunc(self, tagPairA, tagPairB):
-        if tagPairA[1] == tagPairB[1]:  
+        if tagPairA[1] == tagPairB[1]:
             return -1 if tagPairA[0] < tagPairB[0] else 1
         return 1 if tagPairA[1] < tagPairB[1] else -1
 
     def updateTagsRecord(self):
-        self.logger.log("Querying...")
-        results = self.mongo.collection_games.find( {}, projection={settings.rigel.db_keys.tags: True} )
+        self.logger.log("Updating Tags record...")
+        results = self.mongo.collection_games.find({}, projection={settings.gamesMeta.db_keys.tags: True})
 
         tagPairs = {}
         for result in results:
-            for tag in result[settings.rigel.db_keys.tags]:
+            for tag in result[settings.gamesMeta.db_keys.tags]:
                 if tag in tagPairs:
                     tagPairs[tag] = tagPairs[tag] + 1
                 else:
@@ -37,8 +39,8 @@ class UpdateGamesMeta:
         tagPairsArray = []
 
         for key in tagPairs:
-            tagPairsArray.append( (str(key), int(tagPairs[key])) )
-        
+            tagPairsArray.append((str(key), int(tagPairs[key])))
+
         tagPairsArray.sort(key=functools.cmp_to_key(self.sortFunc))
 
         self.logger.log("Found " + str(len(tagPairsArray)) + " unique tags")
@@ -46,12 +48,47 @@ class UpdateGamesMeta:
         entries = []
         for item in tagPairsArray:
             entries.append({
-                "name" : item[0],
-                "count" : item[1]
+                "name": item[0],
+                "count": item[1]
             })
 
         self.logger.log("Updating database...")
-        self.mongo.collection_games_meta.update_one({'_id': "tags"}, {'$set': {"tags" : entries} }, upsert=True)
+        self.mongo.collection_games_meta.update_one({'_id': settings.gamesMeta.db_keys.tags}, {'$set': {settings.gamesMeta.db_keys.tags: entries}}, upsert=True)
+        self.logger.log("Done")
+
+    def updatePopularityIntervals(self):
+        self.logger.log("Updating Popularity intervals...")
+        POPULARITY_FIELD = 'lookupBlob.userRating.ratingCount'
+        results = self.mongo.collection_games.find({}, projection={POPULARITY_FIELD: True})
+
+        allCounts = []
+        min = 99999999
+        max = 0
+        for result in results:
+            count = result['lookupBlob']['userRating']['ratingCount']
+            if count < min:
+                min = count
+            if count > max:
+                max = count
+            allCounts.append(count)
+
+        allCounts.sort(reverse=True)
+
+        maxSize = 6
+        intervals=[max]
+        currentSize=0
+        for count in allCounts:
+            currentSize += 1
+            if currentSize >= maxSize:
+                if count != intervals[0]:
+                    intervals.insert(0, count)
+                    currentSize = 0
+                    if count == 4:
+                        break
+
+        self.logger.log("Processed " + str(len(intervals)) + " intervals")
+        self.logger.log("Updating database...")
+        self.mongo.collection_games_meta.update_one({'_id': settings.gamesMeta.db_keys.popularity_intervals}, {'$set': {settings.gamesMeta.db_keys.popularity_intervals: intervals}}, upsert=True)
         self.logger.log("Done")
 
 if __name__ == '__main__':
