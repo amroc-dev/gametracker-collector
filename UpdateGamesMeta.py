a4d0a749e992cbd752243c8f044dbcdfbe0bc940
Marco <>
2020-12-03 14:40:47 +0000

    Added releaseYears to updateGamesMeta

--- UpdateGamesMeta.py
+++ UpdateGamesMeta.py
@@ -9,6 +9,8 @@ from Mongo import Mongo
 from Shared import settings, hasTestArgs
 import math
 import Helpers
+import collections
+from Helpers import objectKeyFromDotString
 
 class UpdateGamesMeta:
     def __init__(self, mongo):
@@ -16,8 +18,9 @@ class UpdateGamesMeta:
         self.mongo = mongo
 
     def start(self):
-        self.updateTagsRecord()
-        self.updatePopularityIntervals()
+        # self.updateTagsRecord()
+        # self.updatePopularityIntervals()
+        self.updateReleaseYears()
 
     def sortFunc(self, tagPairA, tagPairB):
         if tagPairA[1] == tagPairB[1]:
@@ -61,35 +64,88 @@ class UpdateGamesMeta:
         POPULARITY_FIELD = 'lookupBlob.userRating.ratingCount'
         results = self.mongo.collection_games.find({}, projection={POPULARITY_FIELD: True})
 
+        MIN_POPULARITY = 5
+        countAll = 0
+        countMinOrMore = 0
         allCounts = []
-        min = 99999999
-        max = 0
+        tallies = {}
+
         for result in results:
-            count = result['lookupBlob']['userRating']['ratingCount']
-            if count < min:
-                min = count
-            if count > max:
-                max = count
-            allCounts.append(count)
+            pop = result['lookupBlob']['userRating']['ratingCount']
+            if pop >= MIN_POPULARITY:
+                countMinOrMore += 1
+            countAll += 1
+            allCounts.append(pop)
+            if not pop in tallies:
+                tallies[pop] = 0
+    
+            tallies[pop] += 1
 
-        allCounts.sort(reverse=True)
+        tallies = collections.OrderedDict(sorted(tallies.items()))
 
-        maxSize = 6
-        intervals=[max]
-        currentSize=0
-        for count in allCounts:
-            currentSize += 1
-            if currentSize >= maxSize:
-                if count != intervals[0]:
-                    intervals.insert(0, count)
-                    currentSize = 0
-                    if count == 4:
-                        break
+        buckets = [(MIN_POPULARITY,0)]
+
+        countLeft = countMinOrMore 
+        for key in tallies:
+            divisor = 100 - len(buckets)
+            maxBucketCount = countLeft / divisor if divisor > 0 else countAll
+            
+            if key < MIN_POPULARITY:
+                continue
+            val = tallies[key]
+            if buckets[-1][1] >= maxBucketCount:
+                buckets.append( (key, 0) )
+
+            countLeft -= val  
+
+            buckets[-1] = (buckets[-1][0], buckets[-1][1] + val)
+
+        bucketIndex = 1
+        for bucket in buckets:
+            self.logger.log(str(bucketIndex) + ":" + str(bucket))
+            bucketIndex += 1
+
+        intervals = []
+        for bucket in buckets:
+            intervals.append(bucket[0])
 
         self.logger.log("Processed " + str(len(intervals)) + " intervals")
         self.logger.log("Updating database...")
         self.mongo.collection_games_meta.update_one({'_id': settings.gamesMeta.db_keys.popularity_intervals}, {'$set': {settings.gamesMeta.db_keys.popularity_intervals: intervals}}, upsert=True)
         self.logger.log("Done")
+
+    def updateReleaseYears(self):
+        self.logger.log("Updating release years...")
+        FIELD = 'lookupBlob.releaseDate'
+        results = self.mongo.collection_games.find({}, projection={FIELD: True})
+
+        SANITY_MIN = 2000
+        SANITY_MAX = 2050
+        minYear = 3000
+        maxYear = 0
+        for result in results:
+            releaseDate = objectKeyFromDotString(result, FIELD)
+            if len(releaseDate) > 4:
+                year = int(releaseDate[0:4])
+                if year > SANITY_MIN and year < SANITY_MAX:
+                    if year < minYear:
+                        minYear = year
+                    elif year > maxYear:
+                        maxYear = year
+
+
+
+        if not (minYear > SANITY_MIN and minYear < SANITY_MAX and maxYear > minYear and maxYear < SANITY_MAX):
+            self.logger.log("Years didn't parse sanity check. min:" + str(minYear) + ", max:" + str(maxYear))
+            sys.exit(1)
+        
+        self.logger.log("Range: " + str(minYear) + " to " + str(maxYear));
+        self.logger.log("Updating database...")
+        entries = [minYear, maxYear]
+        self.mongo.collection_games_meta.update_one({'_id': settings.gamesMeta.db_keys.releaseYears}, {'$set': {settings.gamesMeta.db_keys.releaseYears: entries}}, upsert=True)
+        self.logger.log("Done")
+
+
 
 if __name__ == '__main__':
     mongo = Mongo()
